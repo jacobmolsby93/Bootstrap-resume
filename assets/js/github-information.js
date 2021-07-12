c67d7ae6c5065bb8150e4dcae1f58c8823a582a4
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-12 17:07:25 +0000

    Add search for repo function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,19 +1,41 @@
 function userInformationHTML(user) {
     // user is the object returned from the github API.
     return `
-    <h2>${user.name}
-        <span class="small-name">
-            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
-        </span>
-    </h2>
-    <div class="gh-content">
-        <div class="gh-avatar">
-            <a href="${user.html_url}" target="_blank">
-                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-            </a>
-        </div>
-        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-    </div>`;
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    } 
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>                
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -34,22 +56,25 @@ function fetchGitHubInformation(event) {
     // .catch catches errors in the promise.
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        // display user from username
+        $.getJSON(`https://api.github.com/users/${username}`),
+        // list repositorys of user from username
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function (response) {
-            var userData = response;
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse;
+            var repoData = secondResponse;
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(userInformationHTML(repoData));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
-                $("gh-user-data").html(
-                    `<h2>No Info Found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
-                $("gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
-                );
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
-
 }
